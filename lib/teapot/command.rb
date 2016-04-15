2de7b8a96522cfff0dfc080e6f76e79ce19d6634
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2016-04-16 01:08:38 +1200

    Tidy up command line handling.

--- lib/teapot/command.rb
+++ lib/teapot/command.rb
@@ -33,39 +33,10 @@ require 'trollop'
 
 module Teapot
 	# This module implements all top-level teapot commands.
-	module Command
-		def self.parse(arguments)
-			options = Trollop::options(arguments) do
-				banner Rainbow("Teapot: a decentralised package manager and build tool.").bright.blue
-				version "teapot v#{Teapot::VERSION}"
-				
-				opt :configuration, "Specify a specific build configuration.", :type => :string
-				
-				opt :limit, "Limit build to <i> concurrent processes at once where possible", :type => :integer
-				
-				opt :only, "Only compiled direct dependencies."
-				opt :continuous, "Run the build graph continually (experimental).", :type => :boolean, :short => :n
-				
-				opt :in, "Work in the given directory.", :type => :string, :default => Dir.getwd
-				opt :unlock, "Don't use package lockfile when fetching."
-				
-				opt :force, "Force the operation if it would otherwise be be stopped due to a warning."
-				
-				opt :verbose, "Verbose output and error backtraces.", :type => :boolean
-				opt :version, "Print version and exit", :short => :none
-				
-				opt :help, "Show this message"
-			end
-			
-			action = arguments.shift
-			
-			self.new(action, options, arguments)
-		end
-		
-		def initialize(action, options, arguments)
-			@action = action
+	class Command
+		def initialize(arguments, options)
+			@action, *@arguments = arguments
 			@options = options
-			@arguments = arguments
 		end
 		
 		def controller(root = nil)
@@ -73,6 +44,10 @@ module Teapot
 		end
 		
 		def invoke
+			if @action == "invoke"
+				fail "The meaning of life is 42."
+			end
+			
 			self.send(@action)
 		end
 		
