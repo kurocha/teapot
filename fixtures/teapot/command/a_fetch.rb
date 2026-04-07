16c795adc6c17ac3574b6cd320a4afed10a7b2ba
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-04-07 17:58:08 +1200

    Immutable fetch test.

--- fixtures/teapot/command/a_fetch.rb
+++ fixtures/teapot/command/a_fetch.rb
@@ -6,10 +6,18 @@
 require "sus/shared"
 require "build/files"
 
+require "sus/fixtures/temporary_directory_context"
+
 module Teapot
 	module Command
-		AFetch = Sus::Shared("teapot/command/fetch fixture") do
-			let(:root) {::Build::Files::Path.new(File.expand_path("fetch", __dir__))}
+		AFetch = Sus::Shared("a fetch") do
+			include Sus::Fixtures::TemporaryDirectoryContext
+			
+			let(:fetch_root) {::Build::Files::Path.new(File.expand_path("fetch", __dir__))}
+			
+			before do
+				FileUtils.cp_r(File.join(fetch_root, "test-project"), root)
+			end
 		end
 	end
 end
--- test/teapot/command/fetch.rb
+++ test/teapot/command/fetch.rb
@@ -7,11 +7,11 @@ require "teapot/command"
 require "teapot/command/a_fetch"
 
 describe Teapot::Command::Fetch do
-	include_context Teapot::Command::AFetch
+	include Teapot::Command::AFetch
 	
-	let(:project_path) {root + "test-project"}
-	let(:thing_path) {root + "repositories/thing"}
-	let(:thing_package_path) {project_path + "teapot/packages/test/thing"}
+	let(:project_path) {File.join(root, "test-project")}
+	let(:thing_path) {File.join(root, "repositories/thing")}
+	let(:thing_package_path) {File.join(project_path, "teapot/packages/test/thing")}
 	
 	let(:top) {Teapot::Command::Top["--root", project_path.to_s]}
 	
@@ -19,71 +19,36 @@ describe Teapot::Command::Fetch do
 		it "should delete all packages" do
 			top["clean"].call
 			
-			expect(File).not.to be(:exist?, root + "test-project/teapot/packages/test")
+			expect(File).not.to be(:exist?, File.join(root, "test-project/teapot/packages/test"))
 		end
 		
 		it "can create thing repository" do
-			(thing_path + ".git").delete
 			system("git", "init", "-b", "main", chdir: thing_path)
 			system("git", "add", "teapot.rb", chdir: thing_path)
 			system("git", "commit", "-m", "Teapot file for testing", chdir: thing_path)
 		end
-		
-		it "should delete the lock file" do
-			lockfile_path = root + "test-project/test-lock.yml"
-			lockfile_path.delete
-			
-			expect(File).not.to be(:exist?, lockfile_path)
-		end
 	end
 	
-	with "initial fetch" do
+	with "fetch" do
 		let(:subject) {top["fetch"]}
 		
 		it "should fetch repositories" do
 			subject.call
 			
 			# Did the thing package checkout correctly?
-			expect(File).to be(:exist?, root + "test-project/teapot/packages/test/thing/teapot.rb")
+			expect(File).to be(:exist?, File.join(root, "test-project/teapot/packages/test/thing/teapot.rb"))
 		end
 		
-		it "should fetch repositories with no changes" do
-			subject.call
-			
-			# Did the thing package checkout correctly?
-			expect(File).to be(:exist?, root + "test-project/teapot/packages/test/thing/teapot.rb")
-		end
-	end
-	
-	with "fetch with worktree modifications" do
-		let(:subject) {top["fetch"]}
-		let(:path) {root + "test-project/teapot/packages/test/thing/readme.md"}
-		
-		it "can make local modifications" do
-			path.write("Hello World")
-			
-			expect(File).to be(:exist?, path)
-		end
+		let(:local_path) {File.join(root, "test-project/teapot/packages/test/thing/readme.md")}
 		
 		it "can't fetch with local modifications" do
-			expect{subject.call}.to raise_exception(Teapot::Command::FetchError, message: be =~ /local modifications/)
-			
-			path.delete
-		end
-	end
-	
-	with "fetch with upstream changes" do
-		let(:subject) {top["fetch", "--update"]}
-		
-		it "can commit upstream changes" do
-			system("git", "add", "readme.md", chdir: thing_path)
-			system("git", "commit", "-m", "Add documentation", chdir: thing_path)
-		end
-		
-		it "can fetch changes" do
 			subject.call
 			
-			expect(File).to be(:exist?, thing_package_path + "readme.md")
+			File.write(local_path, "Hello World")
+			
+			expect{subject.call}.to raise_exception(Teapot::Command::FetchError, message: be =~ /local modifications/)
+			
+			File.delete(path)
 		end
 	end
 end
